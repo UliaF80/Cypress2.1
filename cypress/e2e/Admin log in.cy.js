46bf04e45fa792dd6f0c47f05a7f44c46d6806fe
UliaF80 <uliafedorova297@cmail.com>
2023-05-16 20:35:34 +0300

    внесены правки

--- cypress/e2e/Admin log in.cy.js
+++ cypress/e2e/Admin log in.cy.js
@@ -3,48 +3,54 @@ const sets = require("../fixtures/chairs");
 
 describe("Admin login", () => {
   it("correct input", () => {
-    cy.visit("/");
-
-    cy.login("qamid@qamid.ru", "qamid");
+    cy.visit("/")
+    cy.login("qamid@qamid.ru", "qamid")
+    cy.get(".page-header > .page-header__subtitle")
+    cy.contains('Администраторррская').should('be.visible')
   });
   it("invalid entry", () => {
     cy.visit("/");
     cy.login("qamid@qami.ru", "qamid");
-    cy.get("body");
+    cy.get("body").contains("Ошибка авторизации!").should("be.visible");
   });
-});
 
-describe("Movie selection", () => {
-  sets.forEach((current) => {
-    it("reserv ticket", () => {
-      cy.visit("/");
-      cy.login("qamid@qamid.ru", "qamid");
 
-      cy.get(selectors.movieTitle)
-        .then(($el) => $el.textContent)
-        .should("have.text", "Терминатор-заржавел");
+  describe("Movie selection", () => {
+    sets.forEach((current) => {
+      it("admin title", () => {
+        cy.visit("/");
+        cy.login("qamid@qamid.ru", "qamid");
 
-      cy.get(selectors.movieTitle)
-        .invoke("text")
-        .then((text) => {
-          cy.visit("http://qamid.tmweb.ru");
-          cy.get(selectors.movieTitle2)
-            .then(($el) => $el.textContent)
-            .should("have.text", text);
+        cy.get(selectors.movieTitle)
+          .then(($el) => $el.textContent)
+          .should("have.text", "Терминатор-заржавел");
+      
+    
 
-          cy.get(".page-nav__day:nth-of-type(3)").click();
-          cy.get("li").contains("10:00").click();
+        cy.get(selectors.movieTitle)
+          .invoke("text")
+          .then((text) => {
+            cy.visit("http://qamid.tmweb.ru");
+            cy.get(selectors.movieTitle2)
+              .then(($el) => $el.textContent)
+              .should("have.text", text);
 
-          current.data.forEach((chair) => {
-            cy.get(
-              `.buying-scheme__wrapper > :nth-child(${chair.row}) > :nth-child(${chair.chair})`
-            ).click();
+            cy.get(".page-nav__day:nth-of-type(3)").click();
+            cy.get("li").contains("10:00").click();
+
+            current.data.forEach((chair) => {
+              cy.get(`.buying-scheme__wrapper > :nth-child(${chair.row}) > :nth-child(${chair.chair})`)
+                .click()
+            
+              cy.contains("Забронировать").click();
+              cy.get(".ticket__check-title").contains("Вы выбрали билеты:").should("be.visible")
+            });
           });
-        });
+      });
     });
   });
-});
 
+});
 describe("Correct display of the main page", () => {
   it("allows you to choose 7 days", () => {
     cy.visit("http://qamid.tmweb.ru");
--- cypress/e2e/Api.cy.js
+++ cypress/e2e/Api.cy.js
@@ -32,18 +32,51 @@ describe("petstore api", () => {
 
         })
     })
-})
+}) 
 
-describe('DELETE Petstore Api', () => {
-    it('Delete pet', () => {
-        cy.request('DELETE', 'https://petstore.swagger.io/v2/pet/123456123')
-            .then((response) => {
-                // expect(response.status).to.be.eql(200)
+describe("petstore api", () => {
+    it('creates pet', () => {
+        cy.request({
+            url: `https://petstore.swagger.io/v2/pet`,
+            method: 'POST',
+            body: {
+                id: 123456,
+                name: 'cats',
+                photoUrls: []
+            }
+        })
+            cy.request({
+                url: `https://petstore.swagger.io/v2/pet`,
+                method: 'PUT',
+                body: {
+                    id: 123456,
+                    name: 'maus',
+                    photoUrls: []
+                }
+            }).then((response) => {
+                expect(response.status).to.be.eql(200)
                 expect(response.body).to.be.eql({
-                    code: 200,
-                    type: 'unknown',
-                    message: '123456123'
+                    id: 123456,
+                    name: 'maus',
+                    photoUrls: [],
+                    tags: []
                 })
             })
+           
+            })
+        })
+    
+
+
+    describe('DELETE Petstore Api', () => {
+        it('Delete pet', () => {
+            cy.request('DELETE', 'https://petstore.swagger.io/v2/pet/123456123')
+                .then((response) => {
+                    expect(response.body).to.be.eql({
+                        code: 200,
+                        type: 'unknown',
+                        message: '123456123'
+                    })
+                })
+        })
     })
-})
