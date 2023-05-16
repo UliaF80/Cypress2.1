9d277a4ef92a1b6d0592187b9ccff754586b3c23
UliaF80 <uliafedorova297@cmail.com>
2023-05-16 20:46:25 +0300

    add *

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "nv5eaj",
+  projectId: "j1v2om",
   e2e: {
     baseUrl: "http://qamid.tmweb.ru/admin",
     setupNodeEvents(on, config) {
